02cef3f1d2de4f0c88732f06a155827554a57754
3thibaut1304 <thibaut.husser@free.fr>
2025-03-26 21:10:41 +0100

    add status online/offline and version of the card

--- src/components/services/Portainer.vue
+++ src/components/services/Portainer.vue
@@ -12,6 +12,17 @@
       </p>
     </template>
     <template #indicator>
+      <div class="notifs">
+        <strong v-if="running > 0" class="notif running" title="Running">
+          {{ running }}
+        </strong>
+        <strong v-if="dead > 0" class="notif dead" title="Dead">
+          {{ dead }}
+        </strong>
+        <strong v-if="misc > 0" class="notif misc" title="Other (creating, paused, exited, etc.)">
+          {{ misc }}
+        </strong>
+      </div>
       <div v-if="status" class="status" :class="status">
         {{ status }}
       </div>
@@ -33,32 +44,93 @@ export default {
     item: Object,
   },
   data: () => ({
+    endpoints: null,
+    containers: null,
     fetchOk: null,
     versionstring: null,
   }),
   computed: {
+    running: function () {
+      if (!this.containers) {
+        return "";
+      }
+      return this.containers.filter((container) => {
+        return container.State.toLowerCase() === "running";
+      }).length;
+    },
+    dead: function () {
+      if (!this.containers) {
+        return "";
+      }
+      return this.containers.filter((container) => {
+        return container.State.toLowerCase() === "dead";
+      }).length;
+    },
+    misc: function () {
+      if (!this.containers) {
+        return "";
+      }
+      return this.containers.filter((container) => {
+        return (
+          container.State.toLowerCase() !== "running" &&
+          container.State.toLowerCase() !== "dead"
+        );
+      }).length;
+    },
     status: function () {
       return this.fetchOk ? "online" : "offline";
     },
   },
   created() {
     this.fetchStatus();
+    this.fetchVersion();
   },
   methods: {
     fetchStatus: async function () {
       const headers = {
         "X-Api-Key": this.item.apikey,
-      }
-      console.log(this.item.url);
+      };
 
+      this.endpoints = await this.fetch("/api/endpoints", { headers }).catch(
+        (e) => {
+          console.error(e);
+        },
+      );
+
+      let containers = [];
+      for (let endpoint of this.endpoints) {
+        if (
+          this.item.environments &&
+          !this.item.environments.includes(endpoint.Name)
+        ) {
+          continue;
+        }
+        const uri = `/api/endpoints/${endpoint.Id}/docker/containers/json?all=1`;
+        const endpointContainers = await this.fetch(uri, { headers }).catch(
+          (e) => {
+            console.error(e);
+          },
+        );
+
+        if (endpointContainers) {
+          containers = containers.concat(endpointContainers);
+        }
+      }
+
+      this.containers = containers;
+    },
+    fetchVersion: async function () {
+      const headers = {
+        "X-Api-Key": this.item.apikey,
+      };
       this.fetch("/api/status", { headers })
         .then((response) => {
           this.fetchOk = true;
           this.versionstring = response.Version;
         })
         .catch((e) => {
-            this.fetchOk = false;
-            console.error(e);
+          this.fetchOk = false;
+          console.error(e);
         });
     },
   },
@@ -92,6 +164,35 @@ export default {
     margin-right: 10px;
     border: 1px solid #000;
     border-radius: 7px;
+  }
+}
+
+.notifs {
+  position: absolute;
+  color: white;
+  font-family: sans-serif;
+  top: 0.3em;
+  right: 0.5em;
+
+  .notif {
+    display: inline-block;
+    padding: 0.2em 0.35em;
+    border-radius: 0.25em;
+    position: relative;
+    margin-left: 0.3em;
+    font-size: 0.8em;
+
+    &.running {
+      background-color: #4fd671;
+    }
+
+    &.dead {
+      background-color: #e51111;
+    }
+
+    &.misc {
+      background-color: #2ed0c8;
+    }
   }
 }
 </style>
